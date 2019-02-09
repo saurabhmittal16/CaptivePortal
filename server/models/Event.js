7b9a4d7e57bd8825aadcd01d7af6c2baee53ceb4
Paramvir Singh <paramvir109@gmail.com>
2019-02-09 21:27:33 +0530

    Created and refactored event and place schema

--- server/models/Event.js
+++ server/models/Event.js
@@ -2,12 +2,23 @@ const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
     name: String,
-    location: String,
+    location : {
+        type : mongoose.Schema.Types.ObjectId
+    },
     details: String,
     time: String,
     category: String
 }, {
     versionKey: false
 });
+
+
+EventSchema.statics.findPlaceByName = async function(eventName) {
+    const Event = this;   
+    const event = await Event.findOne({
+       name : eventName
+    })
+    return event.location; 
+}
 
 module.exports = mongoose.model('event', EventSchema);
