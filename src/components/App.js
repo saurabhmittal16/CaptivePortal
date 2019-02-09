7a7f112fdbe6429546378d8270ba78bfb1ef1e65
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-02-09 19:12:38 +0530

    Initialize boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import ChatBot from './ChatBot';
+import Home from './Home';
+
+class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/chatbot' component={ChatBot} />
+                    <Route path='/' component={Home} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/ChatBot.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ChatBot extends React.Component {
+    render() {
+        return (
+            <h1>Chatbot</h1>
+        );
+    }
+}
+
+export default ChatBot;
